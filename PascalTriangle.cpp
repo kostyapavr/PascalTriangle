1cf7fa9374d1003ed56ebf7b12b5a92dced62cfe
kostyapavr <kostya.pavlenko05@gmail.com>
2024-10-04 18:01:19 +0300

    Initial commit

---
+++ PascalTriangle.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите кол-во строк треугольника Паскаля: ";
+    int N;
+    cin >> N;
+
+    for (int i = 0; i <= N; i++) 
+    {
+        for (int r = 0; r < N - i; r++) cout << "  ";
+
+        int val = 1;
+        for (int k = 1; k <= i; k++) {
+            cout << val << "   ";
+            val = val * (i - k) / k;
+        }
+        cout << endl;
+    }
+}
